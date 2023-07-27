1cdd675b7df7646ea478eed4900dbba28883ce2d
WILLIAM AGUIRRE <williamag929@gmail.com>
2023-07-27 15:12:58 -0400

    auth controller

---
+++ ShiftWork.Backend/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShiftWork.Backend.Data;
+using ShiftWork.Backend.DTOs;
+using ShiftWork.Backend.Models;
+
+namespace ShiftWork.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ShiftWorkContext _context;
+        private readonly IMapper _mapper;
+
+        public AuthController(ShiftWorkContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Person>> GetUser(string id)
+        {
+          if (_context.People == null)
+          {
+              return NotFound();
+          }
+            var person = await _context.People.FirstAsync(c=> c.Email == id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return person;
+        }
+    }
+}
