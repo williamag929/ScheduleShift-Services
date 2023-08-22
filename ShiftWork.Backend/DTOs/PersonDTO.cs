3211ca75946c670daa71005c24818a1504ca5079
WILLIAM AGUIRRE <williamag929@gmail.com>
2023-08-22 15:50:17 -0400

    crud

--- ShiftWork.Backend/DTOs/PersonDTO.cs
+++ ShiftWork.Backend/DTOs/PersonDTO.cs
@@ -6,16 +6,15 @@
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string DocumentNumber { get; set; } = string.Empty;
-        public int ManagerId { get; set; }
-        public bool isManager { get; set; }
-        public bool isSchedule { get; set; }
         public string Email { get; set; } = string.Empty;
         public int PhoneNumber { get; set; }
         public string CompanyId { get; set; } = string.Empty;
-        public string MainAddress {get;set;} = string.Empty;
+        public string MainAddress { get; set; } = string.Empty;
         public bool IsActive { get; set; } = true;
-        public string PersonConfig {get;set; } = string.Empty;
-//        public string PrivateKey {get;set;} = string.Empty;
-        public string AvatarImage {get;set;} = string.Empty;
+        public bool isManager { get; set; } = false;
+        public bool isSchedule { get; set; }
+        //        public string PersonConfig {get;set; } = string.Empty;
+        //        public string PrivateKey {get;set;} = string.Empty;
+        //       public string AvatarImage {get;set;} = string.Empty;
     }
 }
