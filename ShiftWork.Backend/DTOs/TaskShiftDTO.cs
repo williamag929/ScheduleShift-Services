5b0267766cedc5fdf88727a338d3303476660e92
WILLIAM AGUIRRE <williamag929@gmail.com>
2023-08-28 13:47:45 -0400

    dto update

--- ShiftWork.Backend/DTOs/TaskShiftDTO.cs
+++ ShiftWork.Backend/DTOs/TaskShiftDTO.cs
@@ -6,8 +6,8 @@
         public string TaskShiftName { get; set; } = string.Empty;
         public string Comment { get; set; } = string.Empty;
         public DateTime TaskDate { get; set; }
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
         public string CompanyId { get; set; } = string.Empty;
     }
 }
